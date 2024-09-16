b55d3787c2fc14c85165288b19e1dc6358ca1fc2
NikolaMitic-Softelm <nikola.mitic@softelm.com>
2024-09-17 00:03:59 +0200

    feature: register services as singleton

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Services\CustomerService;
+use App\Services\CustomerServiceInterface;
+use App\Services\RideService;
+use App\Services\RideServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(RideServiceInterface::class, RideService::class);
+        $this->app->singleton(CustomerServiceInterface::class, CustomerService::class);
     }
 
     /**
