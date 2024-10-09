fda9eaa3deee1703704301ffe67b6dd0e4d65720
Nikola Mitic <mitic.nikola@elfak.rs>
2024-10-09 22:55:21 +0200

    feature: enable create or cancel for non auth users

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('auth/check', [LoginController::class, 'authCheck']);
 Auth::routes();
+Route::prefix('ride')->group(function () {
+    Route::post('', [RideController::class, 'makeRequest']);
+    Route::delete('/{ride}/cancel', [RideController::class, 'cancelRide']);
+});
 Route::middleware( 'auth:sanctum')->group(function () {
     Route::resource('customer', CustomerController::class)->only(['index', 'show', 'update', 'destroy']);
     Route::prefix('customer')->group(function () {
@@ -35,11 +39,9 @@ Route::middleware( 'auth:sanctum')->group(function () {
     });
     Route::prefix('ride')->group(function () {
         Route::get('', [RideController::class, 'index']);
-        Route::post('', [RideController::class, 'makeRequest']);
         Route::put('/{ride}/accept', [RideController::class, 'acceptRide']);
         Route::put('/{ride}/start', [RideController::class, 'startRide']);
         Route::put('/{ride}/end', [RideController::class, 'endRide']);
-        Route::delete('/{ride}/cancel', [RideController::class, 'cancelRide']);
     });
     Route::prefix('vehicle')->group(function () {
         Route::get('/available', [VehicleController::class, 'available']);
